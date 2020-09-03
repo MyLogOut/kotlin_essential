065f1388f72f60cd0fc4d3081cff2324ed78868a
Patrick D <36824550+SuegroLM@users.noreply.github.com>
2020-09-02 22:14:14 -0600

    Initial commit, also including the first part of chapter 2, where different types of variables are seen.

---
+++ src/main/kotlin/chapter2/_02_01/numericDataTypes.kt
@@ -0,0 +1,15 @@
+package main.kotlin.chapter2._02_01
+
+fun main() {
+    var count: Int=0
+    val number: Int=42
+    val otherNumber = 43
+
+    val myLong: Long= 3000000000L
+    val myByte: Byte = 127
+    val myShort: Short= 32767
+
+    val myDouble:Double = 98.6
+    val myFloat: Float =12.2F
+    val alsoDouble = 101.5
+}
